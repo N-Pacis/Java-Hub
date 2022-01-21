a2f4d5534fd7a27ee888c8f6fd4a695fdbf9e0e5
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-21 10:53:41 +0200

    Update Server.java

--- socket-programming/src/socket/Server.java
+++ socket-programming/src/socket/Server.java
@@ -1,7 +1,25 @@
 package socket;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        try{
+            ServerSocket serverSocket = new ServerSocket(6000);
+            Socket socket = serverSocket.accept();
+
+            InputStream infromclient = socket.getInputStream();
+            DataInputStream request =  new DataInputStream(infromclient);
+            System.out.println(request.readUTF());
+
+            OutputStream responsetoClient = socket.getOutputStream();
+            DataOutputStream response = new DataOutputStream(responsetoClient);
+            response.writeUTF("Your request is received");
+        }
+        catch(Exception ex){
+
+        }
     }
 }
