657949102c0483f44a4089a6356c8ce84d01726c
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-21 10:57:55 +0200

    Update Server.java

--- socket-programming/src/socket/Server.java
+++ socket-programming/src/socket/Server.java
@@ -8,6 +8,7 @@ public class Server {
     public static void main(String[] args) throws IOException {
         try{
             ServerSocket serverSocket = new ServerSocket(6000);
+            serverSocket.setSoTimeout(10000);
             Socket socket = serverSocket.accept();
 
             InputStream infromclient = socket.getInputStream();
@@ -19,7 +20,7 @@ public class Server {
             response.writeUTF("Your request is received");
         }
         catch(Exception ex){
-
+            System.out.println(ex.getMessage());
         }
     }
 }
