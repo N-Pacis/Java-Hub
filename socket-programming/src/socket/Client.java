c99573a6d90c5bb8528d7522ecd7213cdf46c701
N-pacis <pacisnkubito@gmail.com>
2022-01-21 11:41:43 +0200

    [Feat]Added dynamic message

--- socket-programming/src/socket/Client.java
+++ socket-programming/src/socket/Client.java
@@ -1,7 +1,23 @@
 package socket;
 
-public class Client {
-    public static void main(String[] args) {
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("192.168.0.110",5000);
+
+        OutputStream outToServer = socket.getOutputStream();
+        DataOutputStream requestToSend = new DataOutputStream(outToServer);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the message to send");
+        String message = scan.nextLine();
+        requestToSend.writeUTF(message);
+        InputStream inFromServer = socket.getInputStream();
+        DataInputStream response = new DataInputStream(inFromServer);
+        System.out.println("Server responded with: "+response.readUTF());
+
+        socket.close();
     }
 }
