a04b82634f6cfefef3b6c90083b6eac20a41f5ea
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-21 11:38:25 +0200

    [Feat] added infinite loop and writing dynamic message

--- socket-programming/src/socket/Server.java
+++ socket-programming/src/socket/Server.java
@@ -3,21 +3,28 @@ package socket;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         try{
-            ServerSocket serverSocket = new ServerSocket(6000);
-            serverSocket.setSoTimeout(10000);
-            Socket socket = serverSocket.accept();
+            ServerSocket serverSocket = new ServerSocket(5000);
+            while(true){
+                Socket socket = serverSocket.accept();
 
-            InputStream infromclient = socket.getInputStream();
-            DataInputStream request =  new DataInputStream(infromclient);
-            System.out.println(request.readUTF());
+                InputStream infromclient = socket.getInputStream();
+                DataInputStream request =  new DataInputStream(infromclient);
+                System.out.println(request.readUTF());
 
-            OutputStream responsetoClient = socket.getOutputStream();
-            DataOutputStream response = new DataOutputStream(responsetoClient);
-            response.writeUTF("Your request is received");
+                OutputStream responsetoClient = socket.getOutputStream();
+                DataOutputStream response = new DataOutputStream(responsetoClient);
+
+                Scanner scan = new Scanner(System.in);
+                String message;
+                System.out.println("Enter the message to send:");
+                message =  scan.nextLine();
+                response.writeUTF(message);
+            }
         }
         catch(Exception ex){
             System.out.println(ex.getMessage());
