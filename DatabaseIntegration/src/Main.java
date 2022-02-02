d19d0e8a7fdef8db4ec6a38772ab6b161fc24201
Nkubito Pacis <pacisnkubito@gmail.com>
2022-02-02 09:28:05 +0200

    Update Main.java

--- DatabaseIntegration/src/Main.java
+++ DatabaseIntegration/src/Main.java
@@ -1,6 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/javaDatabaseConnection";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "pacis123";
+    private static Connection conn = null;
+
+    private static void connectToDatabase(){
+        try{
+            conn = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+            if(conn != null) System.out.println("Connected to the database successfully!");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        connectToDatabase();
     }
 }
