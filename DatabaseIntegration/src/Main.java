edcdd4d07e829d030d30fcb56489f340b95ae0fc
mugishakundasarah <61735426+mugishakundasarah@users.noreply.github.com>
2022-02-04 22:19:27 +0200

    [Feat] added reading and adding information

--- DatabaseIntegration/src/Main.java
+++ DatabaseIntegration/src/Main.java
@@ -1,11 +1,57 @@
+import com.mysql.cj.protocol.Resultset;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class Main {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/javaDatabaseConnection";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "pacis123";
     private static Connection conn = null;
+
+    private static Boolean createTable(){
+        try{
+            Statement statement = conn.createStatement();
+            boolean passed = statement.execute("CREATE TABLE students(id PRIMARY KEY AUTO_INCREMENT, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, gender VARCHAR(50)");
+            if(passed){
+                System.out.println("Table created successfully!");
+                return true;
+            }
+            return false;
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    private static void insertIntoStudents(String firstname, String lastname,String gender){
+        try{
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("INSERT INTO students(firstname,lastname,gender) VALUES('"+firstname+"','"+lastname+"','"+gender+"')");
+
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void displayStudents(){
+        try{
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM students");
+            while (result.next()){
+                System.out.println("Id: "+result.getInt(1)+", Firstname: "+result.getString(2)+", Lastname: "+result.getString(3)+", Gender: "+result.getString(4));
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 
     private static void connectToDatabase(){
         try{
@@ -19,5 +65,9 @@ public class Main {
 
     public static void main(String[] args) {
         connectToDatabase();
+        if(createTable()){
+            insertIntoStudents("pacis","Nkubito","MALE");
+        }
+        displayStudents();
     }
 }
