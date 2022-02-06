082a5b7b344c19d265aec3270d60eb421490564b
Nkubito Pacis <pacisnkubito@gmail.com>
2022-02-06 19:54:17 +0200

    [Fix] fixed some bugs

--- DatabaseIntegration/src/Main.java
+++ DatabaseIntegration/src/Main.java
@@ -12,27 +12,22 @@ public class Main {
     private static final String DB_PASSWORD = "pacis123";
     private static Connection conn = null;
 
-    private static Boolean createTable(){
+    private static void createTable(){
         try{
             Statement statement = conn.createStatement();
-            boolean passed = statement.execute("CREATE TABLE students(id PRIMARY KEY AUTO_INCREMENT, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, gender VARCHAR(50)");
-            if(passed){
-                System.out.println("Table created successfully!");
-                return true;
-            }
-            return false;
+            boolean passed = statement.execute("CREATE TABLE IF NOT EXISTS students(id INT PRIMARY KEY AUTO_INCREMENT, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, gender VARCHAR(50));");
         }
         catch(Exception e){
             System.out.println(e.getMessage());
+            System.exit(-1);
         }
-        return false;
     }
 
     private static void insertIntoStudents(String firstname, String lastname,String gender){
         try{
             Statement statement = conn.createStatement();
             statement.executeUpdate("INSERT INTO students(firstname,lastname,gender) VALUES('"+firstname+"','"+lastname+"','"+gender+"')");
-
+            System.out.println("New record added successfully");
         }
         catch(Exception e){
             System.out.println(e.getMessage());
@@ -65,9 +60,8 @@ public class Main {
 
     public static void main(String[] args) {
         connectToDatabase();
-        if(createTable()){
-            insertIntoStudents("pacis","Nkubito","MALE");
-        }
+        createTable();
+        insertIntoStudents("doe","john","MALE");
         displayStudents();
     }
 }
